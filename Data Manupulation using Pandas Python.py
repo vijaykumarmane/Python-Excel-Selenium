75f37156489e9a515cf8ccf8f8111713a292f2c0
vijaykumarmane <vijaykumar.mane@searce.com>
2016-07-12 20:35:50 +0530

    Update Data Manupulation using Pandas Python.py

--- Data Manupulation using Pandas Python.py
+++ Data Manupulation using Pandas Python.py
@@ -1,3 +1,7 @@
+""" Data manupulation like excel and Importing Exporting data using Pandas.
+And function like IF and VLookup*, adding new column and filling using lambda function like formulaes in excel and
+deleting and updating data, creating new dataframes like new sheet."""
+
 import pandas as pd
 import numpy as np
 
